ccf7ac48a77a0d3fb0c273eefccf132b61d8f45e
Sandeep Kumar <75414160+SandeepKunwr@users.noreply.github.com>
2020-12-03 13:26:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+    var url = "https://api.covid19india.org/data.json"
+
+    $.getJSON(url,function(data){
+        console.log(data)
+
+        var total_active,total_recovered,total_deaths,total_confirmed
+
+        var state = []
+        var confirmed = []
+        var recovered = []
+        var deaths = []
+
+        $.each(data.statewise,function(id,obj){
+            state.push(obj.state)
+            confirmed.push(obj.confirmed)
+            recovered.push(obj.recovered)
+            deaths.push(obj.deaths)
+        })
+
+        console.log(state)
+
+        state.shift()
+        confirmed.shift()
+        recovered.shift()
+        deaths.shift()
+
+        console.log(state)
+
+        total_active = data.statewise[0].active
+        total_confirmed = data.statewise[0].confirmed
+        total_recovered = data.statewise[0].recovered
+        total_deaths = data.statewise[0].deaths
+
+        $("#active").append(total_active)
+        $("#confirmed").append(total_confirmed)
+        $("#recovered").append(total_recovered)
+        $("#deaths").append(total_deaths)
+
+        var myChart = document.getElementById("myChart").getContext("2d")
+
+        var chart = new Chart(myChart,{
+            type: 'bar',
+            data:{
+                labels:state,
+                datasets:[
+                    {
+                        label: "Confirmed Cases",
+                        data: confirmed,
+                        backgroundColor: "#f1c40f",
+                        minBarlength: 100
+
+
+                    },
+
+                    {
+                        label: "Recovered Cases",
+                        data: recovered,
+                        backgroundColor: "#2ecc71",
+                        minBarlength: 100
+
+
+                    },
+
+
+                    {
+                        label: "Deceased",
+                        data: deaths,
+                        backgroundColor: "#e74c3c",
+                        minBarlength: 100
+
+
+                    },
+
+
+
+                ]
+            },
+            options:{}
+        })
+
+     })
+})  
